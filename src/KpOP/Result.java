82dcfeefd55fe4983ceb149392b7128d50503fc0
sagit <sagitov-227@mail.ru>
2020-03-06 17:44:14 +0300

    mhjgfhgfdgnfd

---
+++ src/KpOP/Result.java
@@ -0,0 +1,4 @@
+package KpOP;
+
+public class Result {
+}
---
+++ src/KpOP/main.java
@@ -0,0 +1,20 @@
+package KpOP;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class main {
+    public static  void main (String args[]){
+        Scanner scanner=new Scanner(System.in);
+        int x=15;
+        if (x == 1) {
+            System.out.println("1 bochka");
+        }
+            if (x== 15) {
+                System.out.println("15 bochek");
+            }
+                if (x==3){
+                    System.out.println("3 bochki");
+                }
+            }
+        }
